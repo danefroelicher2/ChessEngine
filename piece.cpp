080c0a4db25084cc090f687435d1739b84742dee
Dane Froelicher <froelicherdane@gmail.com>
2025-05-13 10:56:26 -0400

    x

--- piece.cpp
+++ piece.cpp
@@ -12,7 +12,7 @@ bool Piece::isBasicallyValid(const Position& pos, const Board& board) const {
     
     return true;
 }
-k
+kd
 char Piece::toChar() const {
     char c = ' ';
     switch (type) {
