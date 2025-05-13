22cfd0c475a2c03f31dcc8c78fa478b7f333f73f
Froelicher <dane.froelicher@kroger.com>
2025-05-13 10:27:36 -0400

    no way

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <memory>
+#include <algorithm>
+#include <cmath>
+
+// Forward declarations
+class Board;
+class Piece;
+class Move;
+class Game;
+class Engine;
+
+#endif // MAIN_H
