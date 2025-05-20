d9afe24a6bf9598183f033b925d09b313bb34e92
Dane Froelicher <froelicherdane@gmail.com>
2025-05-20 08:46:44 -0400

    x

--- zobrist.cpp
+++ zobrist.cpp
@@ -84,6 +84,9 @@ uint64_t Zobrist::generateHashKey(const Board& board) {
 }
 
 uint64_t Zobrist::updateHashKey(uint64_t currentKey, const Move& move, const Board& board) {
+    // Add the initialization check that was missing
+    if (!initialized) initialize();
+    
     uint64_t newKey = currentKey;
     
     // Get the piece being moved
