00defce3d2cb58652aea48b5eeb7b1610ecb484f
Dane Froelicher <froelicherdane@gmail.com>
2025-05-27 09:01:18 -0400

    x

---
+++ tactical_tests.h
@@ -0,0 +1,29 @@
+#ifndef TACTICAL_TESTS_H
+#define TACTICAL_TESTS_H
+
+#include "main.h"
+#include "game.h"
+#include "engine.h"
+
+class TacticalTester {
+public:
+    struct TestPosition {
+        std::string fen;
+        std::string description;
+        std::vector<std::string> bestMoves; // Multiple moves might be equally good
+        int maxDepth;
+        int timeLimit; // milliseconds
+    };
+    
+    // Run a suite of tactical tests
+    static bool runTestSuite(Engine& engine);
+    
+    // Test a single position
+    static bool testPosition(Engine& engine, const TestPosition& test);
+    
+private:
+    static std::vector<TestPosition> getBasicTests();
+    static std::vector<TestPosition> getTacticalTests();
+};
+
+#endif // TACTICAL_TESTS_H
