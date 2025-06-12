7378c07fb61829dd67d17e712dbc5c2c64ec8b95
danefroelicher2 <froelicherdane@gmail.com>
2025-06-11 21:36:52 -0400

    x

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/froel/Downloads/Programming Projects/ChessEngine
+# Install script for directory: C:/Users/froel/Downloads/Programming Projects/In Dev/ChessEngine
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/froel/Downloads/Programming Projects/ChessEngine/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/froel/Downloads/Programming Projects/In Dev/ChessEngine/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
